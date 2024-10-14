077c8e87e2956594b847129495a0033077fa1ce3
Facundo <facundoa17@outlook.com>
2024-10-14 20:49:22 -0300

    Se sube esqueleto principal de la app mvc

---
+++ backend/src/main/java/tec/proyecto/guessdastuff/GuessdastuffApplication.java
@@ -0,0 +1,13 @@
+package tec.proyecto.guessdastuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessdastuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessdastuffApplication.class, args);
+	}
+
+}
